90c9261d92cd7f087184e39c8abcbcd8edd2a1d3
Micheal Lanham <cxbxmxcx@gmail.com>
2024-04-28 17:35:25 -0600

    Revert "Fix api key changes"
    This reverts commit 94ed33dab0363332aca404f4486d3b7b65ea2d45.

---
+++ chapter_9/describe_image.py
@@ -0,0 +1,48 @@
+import base64
+
+import requests
+
+
+def describe_image(image_path="animals.png") -> str:
+    """
+    Uses GPT-4 Vision to inspect and describe the contents of the image.
+
+    :param input_path: str, the name of the PNG file to describe.
+    """
+    api_key = "your-api-key-here"
+
+    # Function to encode the image
+    def encode_image(image_path):
+        with open(image_path, "rb") as image_file:
+            return base64.b64encode(image_file.read()).decode("utf-8")
+
+    # Getting the base64 string
+    base64_image = encode_image(image_path)
+
+    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {api_key}"}
+
+    payload = {
+        "model": "gpt-4-turbo",
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "What’s in this image?"},
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
+                    },
+                ],
+            }
+        ],
+        "max_tokens": 300,
+    }
+
+    response = requests.post(
+        "https://api.openai.com/v1/chat/completions", headers=headers, json=payload
+    )
+
+    return response.json()["choices"][0]["message"]["content"]
+
+
+# print(describe_image())
