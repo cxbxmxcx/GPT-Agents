0b9877bd1b8525b233969128b3c63cec56d2c482
Devender Gollapally <devender@startengine.com>
2025-01-07 17:02:40 -0800

    fixed issues with running script

--- chapter_05/SK_connecting.py
+++ chapter_05/SK_connecting.py
@@ -1,44 +1,37 @@
+from semantic_kernel import Kernel
+from service_settings import ServiceSettings
+from services import Service
 import asyncio
 
-import semantic_kernel as sk
-
-selected_service = "OpenAI"
-kernel = sk.Kernel()
-
-service_id = None
-if selected_service == "OpenAI":
-    from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
-
-    api_key, org_id = sk.openai_settings_from_dot_env()
-    service_id = "oai_chat_gpt"
-    kernel.add_service(
-        OpenAIChatCompletion(
-            service_id=service_id,
-            ai_model_id="gpt-3.5-turbo-1106",
-            api_key=api_key,
-            org_id=org_id,
-        ),
-    )
-elif selected_service == "AzureOpenAI":
-    from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
-
-    deployment, api_key, endpoint = sk.azure_openai_settings_from_dot_env()
-    service_id = "aoai_chat_completion"
-    kernel.add_service(
-        AzureChatCompletion(
-            service_id=service_id,
-            deployment_name=deployment,
-            endpoint=endpoint,
-            api_key=api_key,
-        ),
-    )
-
-
-# This function is currently broken
 async def run_prompt():
     result = await kernel.invoke_prompt(prompt="recommend a movie about time travel")
     print(result)
 
+kernel = Kernel()
+service_settings = ServiceSettings.create()
+selectedService = (
+    Service.OpenAI
+    if service_settings.global_llm_service is None
+    else Service(service_settings.global_llm_service.lower())
+)
+service_id = None
+if selectedService == Service.OpenAI:
+    from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
 
-# Use asyncio.run to execute the async function
+    service_id = "default"
+    kernel.add_service(
+        OpenAIChatCompletion(
+            service_id=service_id,
+        ),
+    )
+elif selectedService == Service.AzureOpenAI:
+    from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
+
+    service_id = "default"
+    kernel.add_service(
+        AzureChatCompletion(
+            service_id=service_id,
+        ),
+    )
+
 asyncio.run(run_prompt())
